b5a466dfd955e82d5157d19fefb59d8de8b76bde
Dobrilkin <95078592+Hylk05@users.noreply.github.com>
2025-12-17 19:21:18 +0300

    6

---
+++ 6/2.java
@@ -0,0 +1,10 @@
+for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+        continue; // пропустить четные числа
+        }
+        System.out.println(i);
+    
+    if (i == 7) {
+        break; // выход при i = 7
+        }
+        }
