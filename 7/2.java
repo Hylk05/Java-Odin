f13e453548a6d866e5af41cb781f865fbc404859
Dobrilkin <95078592+Hylk05@users.noreply.github.com>
2025-12-17 19:22:14 +0300

    7

---
+++ 7/2.java
@@ -0,0 +1,6 @@
+int[] arr = {5, 2, 8, 1, 9};
+String str = java.util.Arrays.toString(arr);
+int index = java.util.Arrays.binarySearch(arr, 8);
+java.util.Arrays.sort(arr);
+boolean eq = java.util.Arrays.equals(arr, new int[]{1, 2, 5, 8, 9});
+int comp = java.util.Arrays.compare(arr, new int[]{1, 2, 5, 8, 9});
